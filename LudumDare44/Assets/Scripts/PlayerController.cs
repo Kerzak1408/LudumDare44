d4a22482390eb9868b2abb2e4aff8fdc09b38ea7
EUROPE\luohman <luohman@microsoft.com>
2019-04-27 15:25:56 +0200

    Added basic movement.

---
+++ LudumDare44/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera MainCamera;
+    public float speed = 5f;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition = new Vector3(mousePosition.x, mousePosition.y, 0);
+
+            var direction = mousePosition - transform.position;
+            direction = direction.normalized;
+
+            this.transform.position += Time.deltaTime * direction * speed;
+        }
+	}
+}
